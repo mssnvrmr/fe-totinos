61364b6e2ba84fb124233031b9489ded85d6ce35
Veronica Lopez <veronica.lopez@imaginexdigital.com>
2026-07-28 18:38:46 -0500

    Page routes

---
+++ src/config/routes.ts
@@ -0,0 +1,19 @@
+interface RouterMap {
+  HOME: string;
+  LOG_IN: string;
+  SIGN_UP: string;
+  ORDERS: string;
+  MENU: string;
+  INGREDIENTS: string;
+  USERS: string;
+}
+
+export const ROUTES: RouterMap = {
+  HOME: '/',
+  LOG_IN: '/log-in',
+  SIGN_UP: '/sign-up',
+  ORDERS: '/orders',
+  MENU: '/menu',
+  INGREDIENTS: '/ingredients',
+  USERS: '/users',
+}
