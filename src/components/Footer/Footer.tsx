cd749d56dbf99d532cf0d329242bb9d58454c82f
Veronica Lopez <veronica.lopez@imaginexdigital.com>
2026-07-31 15:00:23 -0500

    Fix footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,9 @@
-import { Box, Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 
 export const Footer = () => {
   return (
-    <Box sx={{ textAlign: 'center' }}>
+    <Stack sx={{ justifyContent: 'center', alignItems: 'center', height: '50px', width: '100%', backgroundColor: 'primary.dark'}}>
       <Typography variant="body1">Copyright 2026 Totino's Pizza</Typography>
-    </Box>
+    </Stack>
   );
 };
