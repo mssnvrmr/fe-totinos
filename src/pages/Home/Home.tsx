378691b246b565e2842bc388f98a11caad1aa025
Veronica Lopez <veronica.lopez@imaginexdigital.com>
2026-08-05 17:30:44 -0500

    Fix instructions for testing

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -27,11 +27,17 @@ export const Home = () => {
           <Typography variant="body1"><span style={{ fontWeight: 'bold', color: '#FFB300' }}>As a Guest</span> you can check the Menu, but won't be able to place an order.</Typography>
         </Stack>
         <Stack sx={{ gap: 1 }}>
-          <Typography variant="body1">Or you can sign up with a new account to test the experience as a <span style={{ fontWeight: 'bold', color: '#FFB300' }}>as new User</span>.</Typography>
+          <Typography variant="body1">Or you can sign up with a new account to test the experience <span style={{ fontWeight: 'bold', color: '#FFB300' }}>as a new User</span>.</Typography>
         </Stack>
-        <Typography variant="h5">Notes:</Typography>
+        <Typography variant="h5">Search by Criteria:</Typography>
         <Stack sx={{ gap: 1 }}>
-          <Typography variant="body1">To keep the app simple, a lot of business decision where made in a way that is not ideal for a production environment.</Typography>
+          <Typography variant="body1"><span style={{ fontWeight: 'bold', color: '#FFB300' }}>Search by Id</span> only works for <span style={{ fontWeight: 'bold', color: '#FFB300' }}>Ingredients</span>. The ID is exposed on purpose to make it easier to test the functionality.</Typography>
+          <Card sx={{ p: 2, mx: 2 }}>
+            <Typography variant="body1"><b>Olives ID:</b> 8fade6dc-6bd1-4f6d-be1b-08ba8de55828</Typography>
+          </Card>
+        </Stack>
+        <Stack sx={{ gap: 1 }}>
+          <Typography variant="body1"><span style={{ fontWeight: 'bold', color: '#FFB300' }}>Search by E-mail</span> only works for <span style={{ fontWeight: 'bold', color: '#FFB300' }}>Users</span>.</Typography>
         </Stack>
       </Stack>
     </Main>
