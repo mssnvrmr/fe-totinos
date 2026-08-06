4addaa2a452307783b91efcdd0405190cf361bb1
Veronica Lopez <veronica.lopez@imaginexdigital.com>
2026-08-05 21:04:36 -0500

    translate to spanish the instructions

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,37 +7,55 @@ export const Home = () => {
   return (
     <Main>
       <PageHeader title="Home" icon={<ImHome3 />} />
-      <Stack sx={{ gap: 2, width: '30%' }}>
-        <Typography variant="h5">Testing the app with different user roles:</Typography>
-        <Stack sx={{ gap: 1 }}>
-          <Typography variant="body1"><span style={{ fontWeight: 'bold', color: '#FFB300' }}>As an Admin</span> log in with the following credentials:</Typography>
-          <Card sx={{ p: 2, mx: 2 }}>
-            <Typography variant="body1"><b>Email:</b> admin@gmail.com</Typography>
-            <Typography variant="body1"><b>Password:</b> 12345678</Typography>
-          </Card>
+      <Stack direction="row" spacing={2} sx={{ justifyContent: 'center' }}>
+        <Stack sx={{ gap: 2, width: '25%' }}>
+          <Typography variant="h5">Probá la app con diferentes roles de usuario:</Typography>
+          <Stack sx={{ gap: 1 }}>
+            <Typography variant="body1"><span style={{ fontWeight: 'bold', color: '#FFB300' }}>Como Admin</span> iniciá sesión con las siguientes credenciales:</Typography>
+            <Card sx={{ p: 2, mx: 2 }}>
+              <Typography variant="body1"><b>Email:</b> admin@gmail.com</Typography>
+              <Typography variant="body1"><b>Contraseña:</b> 12345678</Typography>
+            </Card>
+          </Stack>
+          <Stack sx={{ gap: 1 }}>
+            <Typography variant="body1"><span style={{ fontWeight: 'bold', color: '#FFB300' }}>Como Usuario</span> con pedidos ya realizados, iniciá sesión con las siguientes credenciales:</Typography>
+            <Card sx={{ p: 2, mx: 2 }}>
+              <Typography variant="body1"><b>Email:</b> jan.doe@gmail.com</Typography>
+              <Typography variant="body1"><b>Contraseña:</b> 12345678</Typography>
+            </Card>
+          </Stack>
+          <Stack sx={{ gap: 1 }}>
+            <Typography variant="body1"><span style={{ fontWeight: 'bold', color: '#FFB300' }}>Como Invitado</span> podés ver el Menú, pero no vas a poder realizar un pedido.</Typography>
+          </Stack>
+          <Stack sx={{ gap: 1 }}>
+            <Typography variant="body1">O podés registrarte con una cuenta nueva para probar la experiencia <span style={{ fontWeight: 'bold', color: '#FFB300' }}>como un Usuario nuevo</span>.</Typography>
+          </Stack>
         </Stack>
-        <Stack sx={{ gap: 1 }}>
-          <Typography variant="body1"><span style={{ fontWeight: 'bold', color: '#FFB300' }}>As an User</span> with orders already placed, log in with the following credentials:</Typography>
-          <Card sx={{ p: 2, mx: 2 }}>
-            <Typography variant="body1"><b>Email:</b> jan.doe@gmail.com</Typography>
-            <Typography variant="body1"><b>Password:</b> 12345678</Typography>
-          </Card>
+        <Stack sx={{ gap: 2, width: '25%' }}>
+          <Typography variant="h5">Búsqueda por criterios:</Typography>
+          <Stack sx={{ gap: 1 }}>
+            <Typography variant="body1"><span style={{ fontWeight: 'bold', color: '#FFB300' }}>Buscar por Id</span> solo funciona para <span style={{ fontWeight: 'bold', color: '#FFB300' }}>Ingredientes</span>. El ID está expuesto a propósito para facilitar las pruebas de la funcionalidad.</Typography>
+            <Card sx={{ p: 2, mx: 2 }}>
+              <Typography variant="body1"><b>ID de Olives:</b> 8fade6dc-6bd1-4f6d-be1b-08ba8de55828</Typography>
+            </Card>
+          </Stack>
+          <Stack sx={{ gap: 1 }}>
+            <Typography variant="body1"><span style={{ fontWeight: 'bold', color: '#FFB300' }}>Buscar por E-mail</span> solo funciona para <span style={{ fontWeight: 'bold', color: '#FFB300' }}>Usuarios</span>.</Typography>
+          </Stack>
         </Stack>
-        <Stack sx={{ gap: 1 }}>
-          <Typography variant="body1"><span style={{ fontWeight: 'bold', color: '#FFB300' }}>As a Guest</span> you can check the Menu, but won't be able to place an order.</Typography>
-        </Stack>
-        <Stack sx={{ gap: 1 }}>
-          <Typography variant="body1">Or you can sign up with a new account to test the experience <span style={{ fontWeight: 'bold', color: '#FFB300' }}>as a new User</span>.</Typography>
-        </Stack>
-        <Typography variant="h5">Search by Criteria:</Typography>
-        <Stack sx={{ gap: 1 }}>
-          <Typography variant="body1"><span style={{ fontWeight: 'bold', color: '#FFB300' }}>Search by Id</span> only works for <span style={{ fontWeight: 'bold', color: '#FFB300' }}>Ingredients</span>. The ID is exposed on purpose to make it easier to test the functionality.</Typography>
-          <Card sx={{ p: 2, mx: 2 }}>
-            <Typography variant="body1"><b>Olives ID:</b> 8fade6dc-6bd1-4f6d-be1b-08ba8de55828</Typography>
-          </Card>
-        </Stack>
-        <Stack sx={{ gap: 1 }}>
-          <Typography variant="body1"><span style={{ fontWeight: 'bold', color: '#FFB300' }}>Search by E-mail</span> only works for <span style={{ fontWeight: 'bold', color: '#FFB300' }}>Users</span>.</Typography>
+        <Stack sx={{ gap: 2, width: '25%' }}>
+          <Typography variant="h5">Pedir una pizza:</Typography>
+          <Stack sx={{ gap: 1 }}>
+            <Typography variant="body1">Al <span style={{ fontWeight: 'bold', color: '#FFB300' }}>agregar una pizza nueva</span>, se calcula si hay stock suficiente de cada ingrediente para asegurar que se pueda preparar.</Typography>
+            <Typography variant="body1">Al <span style={{ fontWeight: 'bold', color: '#FFB300' }}>crear una orden de pizza</span>, se actualiza el stock de los ingredientes utilizados.</Typography>
+            <Typography variant="body1">El calculo es quemado, siendo 100g por cada ingrediente y/o extra.</Typography> 
+            <Typography variant="body1"><span style={{ fontWeight: 'bold', color: '#FFB300' }}>Basil y Magic</span> tienen intencionalmente poco stock para activar esta validación. Podés actualizar los stocks como Admin si querés probar su uso.</Typography>
+          </Stack>
+          <Typography variant="h5">Consideraciones:</Typography>
+          <Stack sx={{ gap: 1 }}>
+            <Typography variant="body1">El sitio no es responsive. Debería visualizarse desde una pantalla de escritorio.</Typography>
+            <Typography variant="body1">Se tomaron varias malas decisiones de diseño porque fue un prototipo rápido.</Typography>
+          </Stack>
         </Stack>
       </Stack>
     </Main>
